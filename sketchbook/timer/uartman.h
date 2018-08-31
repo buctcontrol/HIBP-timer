ebd09b6eef9e89dff825e20abeac8becabdceb0b
control <control@71bacc6f-a8cb-4913-9d5f-2814be948738>
2018-08-31 10:32:12 +0000

    1.删除一些版权信息； 2.默认不依赖串口，确保可以打开使用；
    提交人：雷周骏
    审核人：无

--- sketchbook/timer/uartman.h
+++ sketchbook/timer/uartman.h
@@ -1,27 +1,11 @@
-/* 蓝星文件编码格式为gb2312,cp936  */
 /** $Id$ $DateTime$
-*  @file uartman.c
+*  @file uartman.h
 *  @brief 串口管理模块头文件
 *  @version 0.0.1
 *  @since 0.0.1
-*  @author duwei<duwei@bstar.com.cn>
+*  @author lzj@hbike.net>
 *  @date 2009-08-04    create
 */
-/******************************************************************************
-*@note
-Copyright 2007, BeiJing Bluestar Corporation, Limited
-ALL RIGHTS RESERVED
-Permission is hereby granted to licensees of BeiJing Bluestar, Inc. products
-to use or abstract this computer program for the sole purpose of implementing
-a product based on BeiJing Bluestar, Inc. products. No other rights to
-reproduce, use, or disseminate this computer program,whether in part or in
-whole, are granted. BeiJing Bluestar, Inc. makes no representation or
-warranties with respect to the performance of this computer program, and
-specifically disclaims any responsibility for any damages, special or
-consequential, connected with the use of this program.
-For details, see http://www.bstar.com.cn/
-*******************************************************************************/
-
 #ifndef _UARTMAN_H_
 #define _UARTMAN_H_
 
@@ -75,7 +59,7 @@ do{\
 
 #define dbgAssert(x) assert(x)
 
-#define     WITH_UART   1
+//#define     WITH_UART   1
 
 #define UM_DEV_PATH_NAME_232 "/dev/ttyACM0"
 
